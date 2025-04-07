ccb2d1401420263fcd951bcae2e6ed840c56ce55
Mateo755 <mateusz.polska07@gmail.com>
2025-04-07 22:35:00 +0200

    Revert "Keyboard control commit to apply autonomous control"
    This reverts commit 27b3d1b07fdfa3d10d99de497b927d675dc29973.

--- main.py
+++ main.py
@@ -1,49 +1,38 @@
-import pygame
-import sys
+import pygame 
+
+from vehicle_model import VehicleModelKinematic
 from vehicle_sprite import VehicleSprite
-from vehicle_model import VehicleModelKinematic  # <- Twój model dynamiki pojazdu
+
 
 # --- MAIN ---
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 900))
-    pygame.display.set_caption("Symulacja - WASD")
+    screen = pygame.display.set_mode((800, 600))
     clock = pygame.time.Clock()
 
-    # Model i sprite pojazdu
     model = VehicleModelKinematic()
     car = VehicleSprite(model, start_pos=(400, 300))
-
+  
     running = True
     while running:
-        dt = clock.tick(60) / 1000.0  # dt w sekundach
+        dt = clock.tick(60) / 1000.0  # w sekundach
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         keys = pygame.key.get_pressed()
-        throttle_input = 0.0
-        steering_input = 0.0
+        throttle = 0
+        steering = 0
 
-        if keys[pygame.K_w]: throttle_input = 1.0
-        if keys[pygame.K_s]: throttle_input = -1.0
-        if keys[pygame.K_a]: steering_input = 1.0
-        if keys[pygame.K_d]: steering_input = -1.0
-        if keys[pygame.K_SPACE]: car.stop()  # Zatrzymuje wszystko
-        if keys[pygame.K_r]: car.respawn() # Respawn na środku
-        
-
-        car.apply_control_input(throttle_input, steering_input, dt)
+        #car.set_control(throttle, steering)
         car.update(dt)
 
-        # Rysowanie
         screen.fill((255, 255, 255))
         car.draw(screen)
         pygame.display.flip()
 
     pygame.quit()
-    sys.exit()
 
 
 if __name__ == "__main__":
