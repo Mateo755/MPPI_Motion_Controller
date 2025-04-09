6b160f3f2fcac86a71f85d825eb7cdeecf094cbe
Mateo755 <mateusz.polska07@gmail.com>
2025-04-09 12:26:58 +0200

    Created a MPPI Controller Class, without cost function implementation

---
+++ mppi_controller.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+class MppiController:
+    """Klasa implementująca sterownik bazujący na metodzie MPPI"""
+
+    def ___init__(self, model, N=20, K=100, lambda_=1.0, dt=0.05, noise_sigma=(0.1, 1.0)):
+        """ Inicjalizacja parametrów sterowania
+
+        :param model: obiekt klasy VehicleModel
+        :param N: długość horyzontu (w krokach)
+        :param K: liczba trajektorii - rollouts
+        :param lambda_: parametr eksploracji
+        :param dt: krok symulacji
+        :param noise_sigma: wariancje szumu sterowania (dla delta_dot i T_dot)
+        """
+
+        self.model = model
+        self.N = N
+        self.K = K
+        self.lambda_ = lambda_
+        self.dt = dt
+        self.noise_sigma = np.array(noise_sigma)
+
+        self.udim = 2                                   # [delta_dot, T_dot]
+        self.nominal_u = np.zeros((N, self.udim))       # bieżąca trajektoria sterowania
+
+    def simulate_trajectory(self, x0, U):
+        """Symuluje trajektorię dla danego ciągu sterowań"""
+
+        x = np.copy(x0)
+        trajectory = [x.copy()]             # dodaje stan początkowy x0 do trajektorii
+
+        for u in U:
+            dx = self.model.dynamics(x, u)  # wyliczenie pochodnej wektora stanu, -> jak zmienia się stan
+            x = x + self.dt * dx            # całkowanie metodą eulera, aktualizacja stanu
+            trajectory.append(x.copy())
+        return np.array(trajectory)
+    
+    def compute_cost(self, trajectory):
+        """Funkcja kosztu - analiza wygenerowanych trajektorii i ich ocena"""
+        pass
+
+    def control(self, x0):
+        """Zwraca najlepsze sterowanie na podstawie aktualnego stanu"""
+        
+        # Perturbacje sterowania (δu) - Każdy rollout dostaje unikalny zestaw szumów.
+        noises = np.random.normal(0, self.noise_sigma, size=(self.K, self.N, self.udim))
+        
+        # Inicjalizacja wektora funkcji kosztu trajektorii
+        costs = np.zeros(self.K)
+
+        #Monte Carlo rollouts
+        for k in range(self.K):
+            u_k = self.nominal_u + noises[k]
+            trajectory = self.simulate_trajectory(x0, u_k)
+            costs[k] = self.compute_cost[trajectory] 
+
+        
+        # Ważone poprawki do sterowania
+        beta = np.min(costs)                                        # przesunięcie dla stabilności numerycznej
+        weights = np.exp(-1.0 / self.lambda_ * (costs - beta))      # (costs - beta) daje najniższemu kosztowi wagę ≈ 1
+        weights /= np.sum(weights + 1e-6)                           # normalizacja, by wagi dawały sumę ≈ 1
+        
+        delta_u =  np.sum(weights[:, None, None] * noises, axis=0)  # delta_u = sum_k w_k * δu_k  -> reward-weighted perturbations
+        self.nominal_u += delta_u
+
+        # Zastosowanie u0 i przesunięcie trajektorii
+        u_cmd = self.nominal_u[0]
+
+        self.nominal_u[:-1] = self.nominal_u[1:]    # aktualizacja: usunięcie wykorzystanego sterowania (u0) i wstawienie 0.0
+        self.nominal_u[-1] = 0.0
+
+        return u_cmd
